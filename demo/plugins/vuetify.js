2c838e61b8394de044c02975bd8d6bdda506283b
Daniel Schwab <schwab@infai.org>
2026-04-20 13:25:49 +0200

    fix Vuetify treeshaking component registration in demo page

--- demo/plugins/vuetify.js
+++ demo/plugins/vuetify.js
@@ -2,6 +2,6 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import { VTextField } from 'vuetify/lib';
 
-Vue.use(Vuetify, {components: VTextField});
+Vue.use(Vuetify, {components: { VTextField }});
 
 export default new Vuetify({});
